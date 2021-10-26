aa23518714d5e14c2a8e12ac0d1542564aa6b504
Slobodan Moraca <moracaslobodan@gmail.com>
2021-10-26 19:49:17 +0200

    making board with colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react"
 
-function App() {
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
+
+const App = () => {
+
+  const [curentColorArragementHandler, setCurentColorArragementHandler] = useState([])
+
+  const createBoard = () =>{
+    const randomColorArrangment = []
+    for (let i=0; i<width*width; i++){
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangment.push(randomColor)
+    }
+    setCurentColorArragementHandler(randomColorArrangment)
+  }
+
+  useEffect(() => {
+    createBoard()
+
+  },[])
+
   return (
-    <div>
-      <h1>HI</h1>
+    <div className="app">
+      <div className="game">
+        {curentColorArragementHandler.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{backgroundColor: candyColor}}
+            alt={candyColor}
+          />
+        ))}
+      </div>
     </div>
   );
 }
